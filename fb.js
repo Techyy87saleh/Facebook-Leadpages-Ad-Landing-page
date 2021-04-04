9550b50f68e005b57cc38b7ec76ee3458f66dcbc
Techyy87saleh <79599447+Techyy87saleh@users.noreply.github.com>
2021-04-04 08:26:09 +0600

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,53 @@
+let scrollindicator = document.getElementById('scroll-indicator');
+let HeightMax       = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll',()=>{
+
+    let percentage = ((window.scrollY /HeightMax) * 100);
+    scrollindicator.style.width  = `${percentage}%`;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var countDownDate = new Date("April 25, 2021 15:37:00").getTime();
+
+var x = setInterval(()=>{
+
+    // Get today's date and time
+    var now = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
